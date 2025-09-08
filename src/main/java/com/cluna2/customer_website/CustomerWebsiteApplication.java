b006b969e06ad94810d99ffa40235933c16e468c
cluna2 <lunacris2739@gmail.com>
2025-09-08 13:37:15 -0700

    Initial commit

---
+++ src/main/java/com/cluna2/customer_website/CustomerWebsiteApplication.java
@@ -0,0 +1,13 @@
+package com.cluna2.customer_website;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomerWebsiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomerWebsiteApplication.class, args);
+	}
+
+}
