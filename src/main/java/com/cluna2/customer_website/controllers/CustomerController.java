d8bd4e7546db8c1c753c7d11cbb779a25f31b213
cluna2 <lunacris2739@gmail.com>
2025-09-09 17:49:32 -0700

    Add edit and delete endpoints

--- src/main/java/com/cluna2/customer_website/controllers/CustomerController.java
+++ src/main/java/com/cluna2/customer_website/controllers/CustomerController.java
@@ -5,7 +5,8 @@ import com.cluna2.customer_website.services.CustomerService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
 
 import java.util.List;
 
@@ -23,5 +24,49 @@ public class CustomerController {
         model.addAttribute("customerList", customerList);
 
         return "index";
+    }
+
+    @GetMapping("/new")
+    public String showNewCustomerPage(Model model) {
+        Customer customer = new Customer();
+        model.addAttribute("customer", customer);
+        // return "new-customer" view
+        return "new-customer";
+    }
+
+    @GetMapping("/save")
+    public String saveCustomer(
+            @ModelAttribute("customer") Customer customer) {
+        customerService.saveCustomer(customer);
+        return "redirect:/";
+    }
+
+    @GetMapping("/edit/{id}")
+    public ModelAndView showEditCustomerPage(
+            @PathVariable(name = "id") Long id) {
+        ModelAndView mav = new ModelAndView("edit-customer");
+        Customer customer = customerService.getCustomer(id);
+        mav.addObject("customer", customer);
+        return mav;
+    }
+
+    @PostMapping("/update/{id}")
+    public String updateCustomer(
+            @PathVariable("id") Long id,
+            @ModelAttribute("customer") Customer customer, Model model) {
+        if (!id.equals(customer.getId())) {
+            model.addAttribute("message",
+                    "Cannot update, customer id " + customer.getId() +
+                            " doesn't match id to update: " + id + ".");
+            return "error-page";
+        }
+        customerService.saveCustomer(customer);
+        return "redirect:/";
+    }
+
+    @RequestMapping("/delete/{id}")
+    public String deleteCustomer(@PathVariable("id") Long id) {
+        customerService.deleteCustomer(id);
+        return "redirect:/";
     }
 }
